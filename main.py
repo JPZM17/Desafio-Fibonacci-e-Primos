7d24e0960918b03ece3e4282855bb3d85b3b6e71
João Pedro Ziliotto Martinez <86638776+JPZM17@users.noreply.github.com>
2025-01-23 17:54:08 -0300

    Arrumei os primos

--- main.py
+++ main.py
@@ -45,9 +45,9 @@ def numeros_primos(n: int):
   
   num_primos = []
   for i in range(2, n + 1):
-    for j in range(2, int((n/2)) + 1):
+    for j in range(2, int((i / 2)) + 1):
       if i%j == 0:
-        break;
+        break
     else:
       num_primos.append(i)
   
